32b9587ea83e66f60d9091cce4e7465bf3a23ad9
atheeq saqlain <atheeqsaqlain@gmail.com>
2023-06-04 22:40:01 +0530

    syllabus manager added

---
+++ src/components/syllabus/SyllabusList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h5>syllabus list</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SyllabusList",
+};
+</script>
+
+<style scoped></style>
---
+++ src/pages/StudentsManager.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h4>Manage students</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StudentsManager",
+};
+</script>
+
+<style scoped></style>
