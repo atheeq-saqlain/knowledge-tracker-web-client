fbde1958829fc254fcec6ff063ae11ed413a9287
atheeq saqlain <atheeqsaqlain@gmail.com>
2023-06-06 10:02:00 +0530

    question renderer component added

---
+++ src/components/questions/QuestionRender.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h6>math equation test</h6>
+    <p>
+      When \(a \ne 0\), there are two solutions to \(ax^2 + bx + c = 0\) and they are \[x = {-b \pm \sqrt{b^2-4ac} \over
+      2a}.\]
+    </p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "QuestionRender",
+
+  mounted() {
+    console.log("page is mounted");
+    MathJax.typeset();
+  },
+});
+</script>
