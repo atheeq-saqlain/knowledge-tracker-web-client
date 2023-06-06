dcd303236b5c8f5d9e0a826ac86194aae340ba61
atheeq saqlain <atheeqsaqlain@gmail.com>
2023-06-06 09:52:12 +0530

    formatting changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,5 @@
 import { route } from "quasar/wrappers";
-import {
-  createRouter,
-  createMemoryHistory,
-  createWebHistory,
-  createWebHashHistory,
-} from "vue-router";
+import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from "vue-router";
 import routes from "./routes";
 
 /*
@@ -17,11 +12,7 @@ import routes from "./routes";
  */
 
 export default route(function (/* { store, ssrContext } */) {
-  const createHistory = process.env.SERVER
-    ? createMemoryHistory
-    : process.env.VUE_ROUTER_MODE === "history"
-    ? createWebHistory
-    : createWebHashHistory;
+  const createHistory = process.env.SERVER ? createMemoryHistory : process.env.VUE_ROUTER_MODE === "history" ? createWebHistory : createWebHashHistory;
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
